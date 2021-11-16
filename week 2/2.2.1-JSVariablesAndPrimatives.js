8f021e18666ea803ff3e73e174e25214a2339923
asante williams <asantejones@live.com>
2021-11-15 21:50:11 -0600

    2.2.1-JSVariablesAndPrimatives.js

---
+++ week 2/2.2.1-JSVariablesAndPrimatives.js
@@ -0,0 +1,33 @@
+// create a constant variable (const)
+const bunny;
+// create a variable that can be reassigned (let)
+let wd40;
+wd40= "house"
+
+// create three variables and assign them values of different data types (=)
+let gee = "sleep"
+let days = 5;
+let pretty = true;
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log(typeof pretty)
+
+// change the value referenced by a variable (dynamic typing)
+let variable1 = false;
+variable1 = 24;
+// print the type of the variable you just changed (typeof)
+console.log(typeof variable1);
+
+
+// create variables and give them values to complete the sentences that will print
+// ? which variables need to be created?
+// ? what type of data needs to go in each variable?
+let name = "tom"
+let color="blue"
+let number = 5;
+let sunny = false;
+
+console.log("my name is " + name + ", my eyes are " + color + ". my fav number is " + number + "." )
+
+
+
---
+++ week 2/2.2.2-JSVariablesAndPrimitives.js
@@ -0,0 +1,34 @@
+// create a variable called "name" that references a string
+let name = "asante";
+// create a variable called "favoriteSong" that references a string
+let song = "great man";
+
+// create a variable called "wage" that references a number
+let wage = 18
+// create a variable called "age" that references a number
+let age = 23;
+// create a variable called "onlyChild" that references a boolean
+let onlychild = false
+
+// create a variable called "satisfied" that references a boolean
+let satisfied = false;
+// create a variable called "favoriteThing" that references ANY primitive value
+let favoritething = "cars"
+// print the data type of "favoriteThing"
+console.log(typeof favoritething );
+// ! You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+let favoritesong = tik tok
+// ! You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+let favoritething = basketball
+// ! Happy Birthday!
+// Reassign "age" to a new value
+let age = 24
+// ! You got a big raise at work.
+// Reassign "wage" to a new value
+let wage = 20
+// ! Your raise changed your job satisfaction.
+// Assign "satisfied" to true
+let satisfied = true
+// use "console.log()" and "+" to print a sentence that includes two variables
